5a9f731f1bc19450b7426ec15926aba2f2fd6aec
Shummuy <shummuy@gmail.com>
2022-06-01 21:20:26 -0300

    sorteador v2

---
+++ script.js
@@ -0,0 +1,400 @@
+const atletico = {
+  logo: '<img src="assets/tiera/atletico.png" alt=""></img>',
+  nome: 'Atlético de Madrid',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const barcelona = {
+  logo: '<img src="assets/tiera/barcelona.png" alt=""></img>',
+  nome: 'FC Barcelona',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const bayer = {
+  logo: '<img src="assets/tiera/bayer.png" alt=""></img>',
+  nome: 'Bayern München',
+  country: 'Germany',
+  ligue: 'Bundesliga'
+}
+
+const chelsea = {
+  logo: '<img src="assets/tiera/chelsea.png" alt=""></img>',
+  nome: 'Chelsea',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const juventus = {
+  logo: '<img src="assets/tiera/juventus.png" alt=""></img>',
+  nome: 'Juventus',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const liverpool = {
+  logo: '<img src="/assets/tiera/liverpool.png" alt=""></img>',
+  nome: 'Liverpool',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const mancity = {
+  logo: '<img src="assets/tiera/mancity.png" alt=""></img>',
+  nome: 'Manchester City',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const manunited = {
+  logo: '<img src="assets/tiera/manunited.png" alt=""></img>',
+  nome: 'Manchester United',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const psg = {
+  logo: '<img src="/assets/tiera/psg.png" alt=""></img>',
+  nome: 'Paris Saint-Germain',
+  country: 'France',
+  ligue: 'Ligue 1'
+}
+
+const real = {
+  logo: '<img src="assets/tiera/realmadrid.png" alt=""></img>',
+  nome: 'Real Madrid',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const atalanta = {
+  logo: '<img src="assets/tierb/atalanta.png" alt=""></img>',
+  nome: 'Atalanta',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const borussia = {
+  logo: '<img src="assets/tierb/borussia.png" alt=""></img>',
+  nome: 'Borussia Dortmund',
+  country: 'Germany',
+  ligue: 'Bundesliga'
+}
+
+const inter = {
+  logo: '<img src="assets/tierb/inter.png" alt=""></img>',
+  nome: 'Inter',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const leicester = {
+  logo: '<img src="assets/tierb/leicester.png" alt=""></img>',
+  nome: 'Leicester City',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const milan = {
+  logo: '<img src="assets/tierb/milan.png" alt=""></img>',
+  nome: 'Milan',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const rb = {
+  logo: '<img src="assets/tierb/rb.png" alt=""></img>',
+  nome: 'RB Leipzig',
+  country: 'Germany',
+  ligue: 'Bundesliga'
+}
+
+const sevilla = {
+  logo: '<img src="assets/tierb/sevilla.png" alt=""></img>',
+  nome: 'Sevilla',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const sociedad = {
+  logo: '<img src="assets/tierb/sociedad.png" alt=""></img>',
+  nome: 'Real Sociedad',
+  country: 'Germany',
+  ligue: 'La Liga'
+}
+
+const spurs = {
+  logo: '<img src="assets/tierb/spurs.png" alt=""></img>',
+  nome: 'Tottenham Hotspur',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const villareal = {
+  logo: '<img src="assets/tierb/villareal.png" alt=""></img>',
+  nome: 'Villareal CF',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const ajax = {
+  logo: '<img src="assets/tierc/ajax.png" alt=""></img>',
+  nome: 'Ajax',
+  country: 'Holland',
+  ligue: 'Eredivisie'
+}
+
+const arsenal = {
+  logo: '<img src="assets/tierc/arsenal.png" alt=""></img>',
+  nome: 'Arsenal',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const benfica = {
+  logo: '<img src="assets/tierc/benfica.png" alt=""></img>',
+  nome: 'Benfica',
+  country: 'Portugal',
+  ligue: 'Liga Portugal Bwin'
+}
+
+const bilbao = {
+  logo: '<img src="assets/tierc/bilbao.png" alt=""></img>',
+  nome: 'Athtletic Club de Bilbao',
+  country: 'Spain',
+  ligue: 'La Liga'
+}
+
+const everton = {
+  logo: '<img src="assets/tierc/everton.png" alt=""></img>',
+  nome: 'Everton',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const lazio = {
+  logo: '<img src="assets/tierc/lazio.png" alt=""></img>',
+  nome: 'Lazio',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const monaco = {
+  logo: '<img src="assets/tierc/monaco.png" alt=""></img>',
+  nome: 'AS Monaco',
+  country: 'France',
+  ligue: 'Ligue 1'
+}
+
+const napoli = {
+  logo: '<img src="assets/tierc/napoli.png" alt=""></img>',
+  nome: 'Napoli',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const roma = {
+  logo: '<img src="assets/tierc/roma.png" alt=""></img>',
+  nome: 'Roma',
+  country: 'Italy',
+  ligue: 'Serie A'
+}
+
+const westham = {
+  logo: '<img src="assets/tierc/westham.png" alt=""></img>',
+  nome: 'West Ham United',
+  country: 'England',
+  ligue: 'Premier League'
+}
+
+const timesA = [
+  atletico,
+  barcelona,
+  bayer,
+  chelsea,
+  juventus,
+  liverpool,
+  mancity,
+  manunited,
+  psg,
+  real
+]
+
+const timesB = [
+  atalanta,
+  borussia,
+  inter,
+  leicester,
+  milan,
+  rb,
+  sevilla,
+  sociedad,
+  spurs,
+  villareal
+]
+
+const timesC = [
+  ajax,
+  arsenal,
+  benfica,
+  bilbao,
+  everton,
+  lazio,
+  monaco,
+  napoli,
+  roma,
+  westham
+]
+
+const randomTier = [
+  ajax,
+  arsenal,
+  benfica,
+  bilbao,
+  everton,
+  lazio,
+  monaco,
+  napoli,
+  roma,
+  westham,
+  atletico,
+  barcelona,
+  bayer,
+  chelsea,
+  juventus,
+  liverpool,
+  mancity,
+  manunited,
+  psg,
+  real,
+  atalanta,
+  borussia,
+  inter,
+  leicester,
+  milan,
+  rb,
+  sevilla,
+  sociedad,
+  spurs,
+  villareal
+]
+
+const versus = document.querySelector('.vs-hidden')
+const wrapper = document.querySelectorAll('.wrapper-hidden')
+
+function tierA() {
+  var i = Math.floor(Math.random() * timesA.length - 1 + 1)
+  var i2 = Math.floor(Math.random() * timesA.length - 1 + 1)
+  var time1 = timesA[i]
+  var time2 = timesA[i2]
+
+  if (time1 == time2) {
+    tierA()
+  } else {
+    ;(document.getElementById('logo1').innerHTML = time1.logo),
+      (document.getElementById('name1').innerHTML = time1.nome),
+      (document.getElementById('country1').innerHTML = time1.country),
+      (document.getElementById('ligue1').innerHTML = time1.ligue),
+      (document.getElementById('logo2').innerHTML = time2.logo),
+      (document.getElementById('name2').innerHTML = time2.nome),
+      (document.getElementById('country2').innerHTML = time2.country),
+      (document.getElementById('ligue2').innerHTML = time2.ligue)
+  }
+}
+
+function tierB() {
+  var i = Math.floor(Math.random() * timesB.length - 1 + 1)
+  var i2 = Math.floor(Math.random() * timesB.length - 1 + 1)
+  var time1 = timesB[i]
+  var time2 = timesB[i2]
+
+  if (time1 == time2) {
+    tierB()
+  } else {
+    ;(document.getElementById('logo1').innerHTML = time1.logo),
+      (document.getElementById('name1').innerHTML = time1.nome),
+      (document.getElementById('country1').innerHTML = time1.country),
+      (document.getElementById('ligue1').innerHTML = time1.ligue),
+      (document.getElementById('logo2').innerHTML = time2.logo),
+      (document.getElementById('name2').innerHTML = time2.nome),
+      (document.getElementById('country2').innerHTML = time2.country),
+      (document.getElementById('ligue2').innerHTML = time2.ligue)
+  }
+}
+
+function tierC() {
+  var i = Math.floor(Math.random() * timesC.length - 1 + 1)
+  var i2 = Math.floor(Math.random() * timesC.length - 1 + 1)
+  var time1 = timesC[i]
+  var time2 = timesC[i2]
+
+  if (time1 == time2) {
+    tierC()
+  } else {
+    ;(document.getElementById('logo1').innerHTML = time1.logo),
+      (document.getElementById('name1').innerHTML = time1.nome),
+      (document.getElementById('country1').innerHTML = time1.country),
+      (document.getElementById('ligue1').innerHTML = time1.ligue),
+      (document.getElementById('logo2').innerHTML = time2.logo),
+      (document.getElementById('name2').innerHTML = time2.nome),
+      (document.getElementById('country2').innerHTML = time2.country),
+      (document.getElementById('ligue2').innerHTML = time2.ligue)
+  }
+}
+
+function random() {
+  var i = Math.floor(Math.random() * randomTier.length - 1 + 1)
+  var i2 = Math.floor(Math.random() * randomTier.length - 1 + 1)
+  var time1 = randomTier[i]
+  var time2 = randomTier[i2]
+
+  if (time1 == time2) {
+    random()
+  } else {
+    ;(document.getElementById('logo1').innerHTML = time1.logo),
+      (document.getElementById('name1').innerHTML = time1.nome),
+      (document.getElementById('country1').innerHTML = time1.country),
+      (document.getElementById('ligue1').innerHTML = time1.ligue),
+      (document.getElementById('logo2').innerHTML = time2.logo),
+      (document.getElementById('name2').innerHTML = time2.nome),
+      (document.getElementById('country2').innerHTML = time2.country),
+      (document.getElementById('ligue2').innerHTML = time2.ligue)
+  }
+}
+
+function buttonA() {
+  tierA()
+  tagsVisibility()
+  vsVisibility()
+}
+
+function buttonB() {
+  tierB()
+  tagsVisibility()
+  vsVisibility()
+}
+
+function buttonC() {
+  tierC()
+  tagsVisibility()
+  vsVisibility()
+}
+
+function buttonD() {
+  random()
+  tagsVisibility()
+  vsVisibility()
+}
+
+function tagsVisibility() {
+  wrapper.forEach(wrap => {
+    wrap.classList.remove('wrapper-hidden')
+    wrap.classList.add('wrapper')
+  })
+}
+
+function vsVisibility() {
+  versus.classList.remove('vs-hidden')
+  versus.classList.add('vs')
+}
